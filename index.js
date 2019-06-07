b2fa81f5e78f3aecfcaca0c948c2d28d0ba789e6
befecertified <deekor.baribefe@gmail.com>
2019-06-07 18:01:18 +0100

    Made changes to index file.

--- index.js
+++ index.js
@@ -15,34 +15,35 @@
 // });
 
 /**
- * This example demonstrates setting up webhook on Zeit Now platform.
- * Attention: You have to use webhook with Zeit Now only, polling doesn't
- * work.
+ * This example demonstrates setting up webhook
+ * on the Heroku platform.
  */
 
-
-const TOKEN = process.env.TELEGRAM_TOKEN || '832236376:AAGaNyoPiP3fDV6yMuvT7Z511X0nf_uGKzw';
-const TelegramBot = require('../..');
+const TOKEN =
+  process.env.TELEGRAM_TOKEN || "832236376:AAGaNyoPiP3fDV6yMuvT7Z511X0nf_uGKzw";
+const TelegramBot = require("../..");
 const options = {
   webHook: {
-    // Just use 443 directly
-    port: 443
+    // Port to which you should bind is assigned to $PORT variable
+    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+    port: process.env.PORT
+    // you do NOT need to set up certificates since Heroku provides
+    // the SSL certs already (https://<app-name>.herokuapp.com)
+    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
-// You can use 'now alias <your deployment url> <custom url>' to assign fixed
-// domain.
-// See: https://zeit.co/blog/now-alias
-// Or just use NOW_URL to get deployment url from env.
-const url = 'https://smartbot.deekorbaribefe.now.sh' || process.env.NOW_URL;
+// Heroku routes from port :443 to $PORT
+// Add URL of your app to env variable or enable Dyno Metadata
+// to get this automatically
+// See: https://devcenter.heroku.com/articles/dyno-metadata
+const url = process.env.APP_URL || "https://befebot.herokuapp.com:443";
 const bot = new TelegramBot(TOKEN, options);
-
 
 // This informs the Telegram servers of the new webhook.
 // Note: we do not need to pass in the cert, as it already provided
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
-
 // Just to ping!
-bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
+bot.on("message", function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, "I am alive!");
 });
